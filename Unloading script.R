8bee77beefc2f7ca42870c54aaa6de919d16eb53
julianLRP <129118464+julianLRP@users.noreply.github.com>
2023-04-21 16:39:41 +0200

    Add files via upload

---
+++ Unloading script.R
@@ -0,0 +1,20 @@
+unload_libraries <- function() {
+  # Get the list of currently loaded packages
+  loaded_packages <- (.packages(all.available = TRUE))
+  
+  # Define a vector of base packages that should not be unloaded
+  base_packages <- c("base", "datasets", "graphics", "grDevices", "methods", "stats", "utils")
+  
+  # Unload each package one by one
+  for (pkg in loaded_packages) {
+    if (!(pkg %in% base_packages)) {
+      pkg_detach <- paste0("package:", pkg)
+      if (pkg_detach %in% search()) {
+        detach(pkg_detach, unload = TRUE, character.only = TRUE)
+      }
+    }
+  }
+}
+
+# Call the function to unload non-base libraries
+unload_libraries()
