4a3b807c8a64086e3ab4cb1c79355dd2533119f9
julianLRP <129118464+julianLRP@users.noreply.github.com>
2023-06-07 16:29:33 +0200

    Add files via upload

---
+++ shinyprocessing/Image processing shiny.R
@@ -0,0 +1,9 @@
+# Function to read and preprocess the uploaded image
+process_uploaded_image <- function(file) {
+  # ...
+}
+
+# Function to apply the watershed algorithm and label the image
+label_image <- function(img) {
+  # ...
+}
---
+++ shinyprocessing/User interaction.R
@@ -0,0 +1,9 @@
+# Define the UI for the app
+ui <- fluidPage(
+  # ...
+)
+
+# Define the server logic for the app
+server <- function(input, output) {
+  # ...
+}
