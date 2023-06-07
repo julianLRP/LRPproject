6494b00d77df251b080f2160359384fc929600a2
julianLRP <129118464+julianLRP@users.noreply.github.com>
2023-06-07 16:28:49 +0200

    Update App.R

--- shinyprocessing/App.R
+++ shinyprocessing/App.R
@@ -1,13 +1,21 @@
 # Load required packages
 library(shiny)
 library(EBImage)
-
+library(png)
 # Load custom functions and modules
 source("image_processing.R")
 source("user_interaction.R")
 
 # Define the Shiny app
 shinyApp(
-  ui = ui, 
+  ui = ui <- fluidPage(
+    fileInput("upload", "Upload your image", accept = c('image/png', 'image/jpeg')),
+    imageOutput("img"),
+    colourInput("color", "Choose color"),
+    textInput("label", "Enter label for this color"),
+    actionButton("save", "Save label"),
+    tableOutput("labels"),
+    actionButton("save_image", "Save labeled image")
+    , 
   server = server
-)
+))
